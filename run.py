7e466d9574c6d7d14daf4cc8aaac7936959032c7
Rachita Barla <67193552+Rachitabarla@users.noreply.github.com>
2024-06-16 21:23:22 +0530

    Create run.py
    This commit is used to create the run file that implements SSE

---
+++ run.py
@@ -0,0 +1,39 @@
+import asyncio
+from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
+from sse_starlette.sse import EventSourceResponse
+
+app = FastAPI(docs_url='/docs')
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+)
+
+client_queue = {} # keeps track of the multiple client connections
+
+@app.post('/write')
+async def adding_new_line(new_data: str):
+    for each_queue in client_queue.values():
+        await each_queue.put(f"{new_data}")
+    return {"message":"new line added"}
+  
+@app.get('/stream') 
+async def message_stream(request: Request):
+    queue = asyncio.Queue()
+    client_id = id(queue)
+    client_queue[client_id] = queue
+
+    async def event_generator():
+        try:
+          while True:
+              # If client closes connection, stop sending events
+              if await request.is_disconnected():
+                  break
+              try:
+                  event = await queue.get()
+                  yield event
+              except asyncio.CancelledError:
+                  break
+        finally: client_queue.pop(client_id)
+
+    return EventSourceResponse(event_generator())
